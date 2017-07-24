3756c06770d059fe5c77ab8ef8ec96fc26851415
Gareth Tan <garethwtan@gmail.com>
2017-07-23 20:46:09 -0400

    Refactor for JSON converters to take a configuration instance.

--- src/JSONSchema/SchemaConverter.hs
+++ src/JSONSchema/SchemaConverter.hs
@@ -1,8 +1,12 @@
 module JSONSchema.SchemaConverter
   ( jsonToSchema
+  , jsonToSchemaWithConfig
   , jsonsToSchema
+  , jsonsToSchemaWithConfig
   , schemasToSchema
   , unifySchemas
+  , SchemaGenerationConfig(..)
+  , defaultSchemaGenerationConfig
   ) where
 
 import           Protolude
@@ -27,12 +31,23 @@ import qualified Safe.Foldable                     as SF
 
 import qualified Utils
 
+data SchemaGenerationConfig = SchemaGenerationConfig {
+      typeArraysAsTuples :: Bool
+    , sealObjectProperties :: Bool
+}
+
+defaultSchemaGenerationConfig :: SchemaGenerationConfig
+defaultSchemaGenerationConfig = SchemaGenerationConfig {
+    typeArraysAsTuples = False
+  , sealObjectProperties = False
+}
+
 makeBasicTypeSchema :: V4A.SchemaType -> D4.Schema
 makeBasicTypeSchema t =
   D4.emptySchema {D4._schemaType = Just $ V4A.TypeValidatorString t}
 
-makeObjectSchema :: AE.Object -> D4.Schema
-makeObjectSchema o =
+makeObjectSchema :: SchemaGenerationConfig -> AE.Object -> D4.Schema
+makeObjectSchema c o =
   (makeBasicTypeSchema V4A.SchemaObject) {
     D4._schemaRequired = requireds o,
     D4._schemaProperties = properties o
@@ -40,31 +55,39 @@ makeObjectSchema o =
   where
     -- We add the required property only if the object has any keys
     requireds = fmap DS.fromList . Utils.listToMaybeList . HM.keys
-    properties = Just . map jsonToSchema
+    properties :: HM.HashMap Text AE.Value -> Maybe (HM.HashMap Text D4.Schema)
+    properties = Just . map (jsonToSchemaWithConfig c)
 
-makeArrayAsTupleSchema :: AE.Array -> D4.Schema
-makeArrayAsTupleSchema xs =
+makeArrayAsTupleSchema :: SchemaGenerationConfig -> AE.Array -> D4.Schema
+makeArrayAsTupleSchema c xs =
   (makeBasicTypeSchema V4A.SchemaArray) {
-    D4._schemaItems = Just $ V4Arr.ItemsArray $ V.toList $ fmap jsonToSchema xs
+    D4._schemaItems = Just $ V4Arr.ItemsArray $ V.toList $ fmap (jsonToSchemaWithConfig c) xs
   }
 
-makeArrayAsSingleSchema :: AE.Array -> D4.Schema
-makeArrayAsSingleSchema xs =
+makeArrayAsSingleSchema :: SchemaGenerationConfig -> AE.Array -> D4.Schema
+makeArrayAsSingleSchema c xs =
   (makeBasicTypeSchema V4A.SchemaArray) {
     D4._schemaItems =
-      Just $ V4Arr.ItemsObject $ fromMaybe D4.emptySchema $ jsonsToSchema xs
+      Just $ V4Arr.ItemsObject $ fromMaybe D4.emptySchema $ jsonsToSchemaWithConfig c xs
   }
 
+jsonToSchemaWithConfig :: SchemaGenerationConfig -> AE.Value -> D4.Schema
+jsonToSchemaWithConfig c (AE.Number n) = makeBasicTypeSchema (if DS.isInteger n then V4A.SchemaInteger else V4A.SchemaNumber)
+jsonToSchemaWithConfig c (AE.String s) = makeBasicTypeSchema V4A.SchemaString
+jsonToSchemaWithConfig c (AE.Bool s)   = makeBasicTypeSchema V4A.SchemaBoolean
+jsonToSchemaWithConfig c AE.Null       = makeBasicTypeSchema V4A.SchemaNull
+jsonToSchemaWithConfig c (AE.Object o) = makeObjectSchema c o
+jsonToSchemaWithConfig c (AE.Array xs) = makeArrayAsSingleSchema c xs
+
+
 jsonToSchema :: AE.Value -> D4.Schema
-jsonToSchema (AE.Number n) = makeBasicTypeSchema (if DS.isInteger n then V4A.SchemaInteger else V4A.SchemaNumber)
-jsonToSchema (AE.String s) = makeBasicTypeSchema V4A.SchemaString
-jsonToSchema (AE.Bool s)   = makeBasicTypeSchema V4A.SchemaBoolean
-jsonToSchema AE.Null       = makeBasicTypeSchema V4A.SchemaNull
-jsonToSchema (AE.Object o) = makeObjectSchema o
-jsonToSchema (AE.Array xs) = makeArrayAsSingleSchema xs
+jsonToSchema = jsonToSchemaWithConfig defaultSchemaGenerationConfig
 
 schemasToSchema :: (Foldable f, Functor f) => f D4.Schema -> Maybe D4.Schema
 schemasToSchema = SF.foldr1May unifySchemas
 
 jsonsToSchema :: (Foldable f, Functor f) => f AE.Value -> Maybe D4.Schema
-jsonsToSchema = schemasToSchema . fmap jsonToSchema
+jsonsToSchema = jsonsToSchemaWithConfig defaultSchemaGenerationConfig
+
+jsonsToSchemaWithConfig :: (Foldable f, Functor f) => SchemaGenerationConfig -> f AE.Value -> Maybe D4.Schema
+jsonsToSchemaWithConfig c = schemasToSchema . fmap (jsonToSchemaWithConfig defaultSchemaGenerationConfig)
